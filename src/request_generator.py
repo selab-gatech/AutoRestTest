0155ec3c9589e6fcbe9452472b7581451ac8dad2
dshah374@gatech.edu <90000344+DhruvShah09@users.noreply.github.com>
2024-02-17 17:36:02 -0500

    baseline works

--- src/request_generator.py
+++ src/request_generator.py
@@ -3,6 +3,7 @@ import string
 from dataclasses import dataclass
 from typing import List, Dict
 
+import threading
 import requests
 import urllib
 import json
@@ -26,13 +27,14 @@ class StatusCode:
     requests: List[RequestData]
 
 class RequestsGenerator:
-    def __init__(self, file_path: str, api_url: str):
+    def __init__(self, file_path: str, api_url: str, is_local: bool = False):
         self.file_path = file_path
         self.api_url = api_url
         self.successful_query_data: List[RequestData] = [] # list that will store successfuly query_parameters
         self.status_codes: Dict[int: StatusCode] = {} # dictionary to track status code occurrences
         self.specification_parser: SpecificationParser = SpecificationParser(self.file_path)
         self.operations: Dict[str, OperationProperties] = self.specification_parser.parse_specification()
+        self.is_local = is_local
 
     def get_simple_type(self, variable):
         """
@@ -178,7 +180,7 @@ class RequestsGenerator:
                     response = select_method(self.api_url + endpoint_path, params=query_parameters, data=request_body)
             else:
                 response = select_method(self.api_url + endpoint_path, params=query_parameters)
-        except requests.exceptions.RequestException:
+        except requests.exceptions.RequestException as e:
             print("Request failed")
             return None
         return response
@@ -221,7 +223,7 @@ class RequestsGenerator:
         print("Request Sent")
         response = self.send_request(request_data)
         print("Response Recieved")
-        if response:
+        if response is not None:
             self.process_response(response, request_data)
             self.attempt_retry(response, request_data)
 
@@ -231,8 +233,21 @@ class RequestsGenerator:
         """
         print("Generating Request...")
         print()
-        for operation_id, operation_properties in self.operations.items():
-            self.process_operation(operation_properties)
+        if not self.is_local:
+            num_workers = 5
+            worker_queues = [[] for i in range(num_workers)] 
+            for i, (operation_id, operation_properties) in enumerate(self.operations.items()):
+                worker_queues[i % num_workers].append((operation_id, operation_properties))
+            workers = []
+            for i in range(num_workers):
+                worker = threading.Thread(target=self.process_operation, args=(worker_queues[i],))
+                workers.append(worker)
+                worker.start()
+            for worker in workers:
+                worker.join()
+        else:
+            for operation_id, operation_properties in self.operations.items():
+                self.process_operation(operation_properties)
 
         self.mutate_requests()
         print()
@@ -240,9 +255,11 @@ class RequestsGenerator:
 
 #testing code
 if __name__ == "__main__":
-    request_generator = RequestsGenerator(file_path="../specs/original/oas/ocvn.yaml", api_url="http://localhost:50112")
-    request_generator.requests_generate()
+    request_generator = RequestsGenerator(file_path="../specs/original/oas/genome-nexus.yaml", api_url="http://localhost:50110", is_local=True)
+    for i in range(5):
+        request_generator.requests_generate()
+        print(i)
     #generate histogram using self.status_code_counts
-    print(request_generator.status_code_counts)
+    print([(x.status_code, x.count) for x in request_generator.status_codes.values()])
     #for i in range(10):
     #    print(request_generator.randomize_parameter_value())
