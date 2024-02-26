393bb6d8101af3b1bb74878b5f1dba845d0e1a29
tylerstennett <tyler.j.stennett@gmail.com>
2024-02-26 01:02:39 -0500

    Added changes to morest testing

--- tools/morest/fuzzer.py
+++ tools/morest/fuzzer.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 from prance import ResolvingParser
@@ -5,6 +6,7 @@ from prance import ResolvingParser
 from build_graph import parse
 from fuzzer.fuzzer import APIFuzzer
 from utils.auth_util import get_token, SUT
+from config import DEV_SERVER_ADDRESS
 
 
 def default_reclimit_handler(limit, parsed_url, recursions=()):
@@ -31,6 +33,45 @@ def main():
     api_fuzzer = APIFuzzer(apis, parser.specification, odg, test_args[1], time_budget=60)
     api_fuzzer.run()
 
+def run_fuzzer(spec_path, server_url):
+    test_args = [spec_path, server_url, SUT.BITBUCKET]
+    parser = ResolvingParser(test_args[0],
+                             recursion_limit_handler=default_reclimit_handler, backend='openapi-spec-validator',
+                             strict=False)
+    apis, odg = parse(parser.specification)
+    odg.draw()
+    api_fuzzer = APIFuzzer(apis, parser.specification, odg, test_args[1], time_budget=60) # changed time budget from 3600 to 60
+    api_fuzzer.run()
+
+def argument_parse() -> (str, str):
+    service_urls = {
+        'fdic': "http://0.0.0.0:9001",
+        'genome-nexus': "http://0.0.0.0:9002",
+        'language-tool': "http://0.0.0.0:9003",
+        'ocvn': "http://0.0.0.0:9004",
+        'ohsome': "http://0.0.0.0:9005",
+        'omdb': "http://0.0.0.0:9006",
+        'rest-countries': "http://0.0.0.0:9007",
+        'spotify': "http://0.0.0.0:9008",
+        'youtube': "http://0.0.0.0:9009"
+    }
+    parser = argparse.ArgumentParser(description='Generate requests based on API specification.')
+    parser.add_argument('service', help='The service specification to use.')
+    parser.add_argument('is_local', help='Whether the services are loaded locally or not.')
+    args = parser.parse_args()
+    api_url = service_urls.get(args.service)
+    is_local = args.is_local
+    if api_url is None:
+        print(f"Service '{args.service}' not recognized. Available services are: {list(service_urls.keys())}")
+        exit(1)
+    if is_local not in {"true", "false"}:
+        print(f"Invalid value for 'is_local'. Must be either 'true' or 'false'.")
+        exit(1)
+    if is_local == "false":
+        api_url = api_url.replace("0.0.0.0", DEV_SERVER_ADDRESS)  # use config.py for DEV_SERVER_ADDRESS var
+        api_url = api_url.replace(":9", ":8") # use public server proxy ports
+    return args.service, api_url
+
 class MorestFuzzer:
     def __init__(self, spec_path, server_url):
         self.spec_path = spec_path
@@ -39,15 +80,9 @@ class MorestFuzzer:
     def run(self):
         run_fuzzer(self.spec_path, self.server_url)
 
-def run_fuzzer(spec_path, server_url):
-    test_args = [spec_path, server_url, SUT.BITBUCKET]
-    parser = ResolvingParser(test_args[0],
-                             recursion_limit_handler=default_reclimit_handler, backend='openapi-spec-validator',
-                             strict=False)
-    apis, odg = parse(parser.specification)
-    odg.draw()
-    api_fuzzer = APIFuzzer(apis, parser.specification, odg, test_args[1], time_budget=3600)
-    api_fuzzer.run()
-
 if __name__ == '__main__':
-    main()
+    service_name, api_url = argument_parse()
+    file_path = f"../../specs/original/oas/{service_name}.yaml"
+    fuzzer = MorestFuzzer(file_path, api_url)
+    fuzzer.run()
+    #main()
