d68caa3ce1fcac611f04ff20ffd720b48ae67379
tylerstennett <tyler.j.stennett@gmail.com>
2024-02-29 04:16:35 -0500

    Added timer on baseline fuzzer

--- src/request_generator.py
+++ src/request_generator.py
@@ -1,5 +1,6 @@
 import os
 import random
+import time
 from dataclasses import dataclass
 from typing import List, Dict
 import argparse
@@ -33,7 +34,7 @@ class StatusCode:
     requests_and_responses: List[RequestResponse]
 
 class RequestsGenerator:
-    def __init__(self, file_path: str, api_url: str, is_local: bool = True):
+    def __init__(self, file_path: str, api_url: str, is_local: bool = True, time_duration=600):
         self.file_path = file_path
         self.api_url = api_url
         self.successful_query_data: List[RequestData] = [] # list that will store successfuly query_parameters
@@ -41,6 +42,7 @@ class RequestsGenerator:
         self.specification_parser: SpecificationParser = SpecificationParser(self.file_path)
         self.operations: Dict[str, OperationProperties] = self.specification_parser.parse_specification()
         self.is_local = is_local
+        self.time_duration = time_duration
 
     def get_simple_type(self, variable):
         """
@@ -260,43 +262,16 @@ class RequestsGenerator:
             for worker in workers:
                 worker.join()
         else:
-            for operation_id, operation_properties in self.operations.items():
-                for _ in range(10):
-                    self.process_operation(operation_properties)
+            start_time = time.time()
+            while (time.time() - start_time) < self.time_duration:
+                for operation_id, operation_properties in self.operations.items():
+                    for _ in range(5):
+                        self.process_operation(operation_properties)
+                    if (time.time() - start_time) > self.time_duration:
+                        break
 
         self.mutate_requests()
         print("Generated Requests!")
-
-
-
-def argument_parse() -> (str, str):
-    service_urls = {
-        'fdic': "http://0.0.0.0:9001",
-        'genome-nexus': "http://0.0.0.0:9002",
-        'language-tool': "http://0.0.0.0:9003",
-        'ocvn': "http://0.0.0.0:9004",
-        'ohsome': "http://0.0.0.0:9005",
-        'omdb': "http://0.0.0.0:9006",
-        'rest-countries': "http://0.0.0.0:9007",
-        'spotify': "http://0.0.0.0:9008",
-        'youtube': "http://0.0.0.0:9009"
-    }
-    parser = argparse.ArgumentParser(description='Generate requests based on API specification.')
-    parser.add_argument('service', help='The service specification to use.')
-    parser.add_argument('is_local', help='Whether the services are loaded locally or not.')
-    args = parser.parse_args()
-    api_url = service_urls.get(args.service)
-    is_local = args.is_local
-    if api_url is None:
-        print(f"Service '{args.service}' not recognized. Available services are: {list(service_urls.keys())}")
-        exit(1)
-    if is_local not in {"true", "false"}:
-        print(f"Invalid value for 'is_local'. Must be either 'true' or 'false'.")
-        exit(1)
-    if is_local == "false":
-        api_url = api_url.replace("0.0.0.0", DEV_SERVER_ADDRESS)  # use config.py for DEV_SERVER_ADDRESS var
-        api_url = api_url.replace(":9", ":8") # use public server proxy ports
-    return args.service, api_url
 
 def output_responses(request_generator: RequestsGenerator, service_name: str):
     """
@@ -318,12 +293,43 @@ def output_responses(request_generator: RequestsGenerator, service_name: str):
                 file.write(f"Response Text: {request_response.response_text}\n")
                 file.write("\n")
 
+def argument_parse() -> (str, str):
+    service_urls = {
+        'fdic': "http://0.0.0.0:9001",
+        'genome-nexus': "http://0.0.0.0:9002",
+        'language-tool': "http://0.0.0.0:9003",
+        'ocvn': "http://0.0.0.0:9004",
+        'ohsome': "http://0.0.0.0:9005",
+        'omdb': "http://0.0.0.0:9006",
+        'rest-countries': "http://0.0.0.0:9007",
+        'spotify': "http://0.0.0.0:9008",
+        'youtube': "http://0.0.0.0:9009"
+    }
+    parser = argparse.ArgumentParser(description='Generate requests based on API specification.')
+    parser.add_argument('service', help='The service specification to use.')
+    parser.add_argument('is_local', help='Whether the services are loaded locally or not.')
+    parser.add_argument('time_duration', help='The time duration to run the tests for.')
+    args = parser.parse_args()
+    api_url = service_urls.get(args.service)
+    is_local = args.is_local
+    time_duration = args.time_duration
+    if api_url is None:
+        print(f"Service '{args.service}' not recognized. Available services are: {list(service_urls.keys())}")
+        exit(1)
+    if is_local not in {"true", "false"}:
+        print(f"Invalid value for 'is_local'. Must be either 'true' or 'false'.")
+        exit(1)
+    if is_local == "false":
+        api_url = api_url.replace("0.0.0.0", DEV_SERVER_ADDRESS)  # use config.py for DEV_SERVER_ADDRESS var
+        api_url = api_url.replace(":9", ":8") # use public server proxy ports
+    return args.service, api_url, time_duration
+
 #testing code
 if __name__ == "__main__":
-    service_name, api_url = argument_parse()
+    service_name, api_url, time_duration = argument_parse()
     file_path = f"../specs/original/oas/{service_name}.yaml"
     print("Checking requests at: ", api_url)
-    request_generator = RequestsGenerator(file_path=file_path, api_url=api_url, is_local=True)
+    request_generator = RequestsGenerator(file_path=file_path, api_url=api_url, is_local=True, time_duration=time_duration)
     for i in range(1):
         request_generator.requests_generate()
     print([(x.status_code, x.count) for x in request_generator.status_codes.values()])
