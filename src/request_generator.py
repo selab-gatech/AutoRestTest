2faf77ccb895a09445accb386dd89dc7e952ece2
tylerstennett <tyler.j.stennett@gmail.com>
2024-03-25 20:37:41 -0400

    Added some base functionality to value generator

--- src/request_generator.py
+++ src/request_generator.py
@@ -1,33 +1,59 @@
+from dataclasses import dataclass
+from typing import Dict, AnyStr, Set, Any
+
 from .generate_graph import OperationGraph, OperationNode, OperationEdge
-from .specification_parser import OperationProperties
+from .specification_parser import OperationProperties, ParameterProperties, SchemaProperties
 import requests
 
-class RequestGenerator:
+from .value_generator import NaiveValueGenerator, identify_generator
+
+
+@dataclass
+class RequestData:
+    endpoint_path: AnyStr
+    http_method: AnyStr
+    parameter_values: Dict[AnyStr, Any] # dict of parameter name to value
+    request_body: AnyStr
+    content_type: AnyStr
+    operation_id: AnyStr
+    operation_properties: OperationProperties
+
+class NaiveRequestGenerator:
     def __init__(self, operation_graph: OperationGraph):
         self.operation_graph: OperationGraph = operation_graph
 
-    def naive_generate_request(self, operation_properties: OperationProperties):
+    def generate_parameter_values(self, parameters: Dict[AnyStr, ParameterProperties], request_body: Dict[AnyStr, SchemaProperties]):
+        value_generator = NaiveValueGenerator(parameters=parameters, request_body=request_body)
+        return value_generator.generate_parameters() if parameters else None, value_generator.generate_request_body() if request_body else None
+
+    def send_operation_request(self, operation_properties: OperationProperties):
         '''
         Generate naive requests based on the default values and types
         '''
-        pass
+        endpoint_path: AnyStr = operation_properties.endpoint_path
+        for parameter_name, parameter_properties in operation_properties.parameters.items():
+            if parameter_properties.in_value == "path":
+                get_path_value = identify_generator(parameter_properties.schema.type)
+                endpoint_path.replace("{"+parameter_name+"}", get_path_value)
 
-    def naive_depth_traversal(self, curr_node: OperationNode, visited: set):
+        parameters, request_body = self.generate_parameter_values(operation_properties.parameters, operation_properties.request_body)
+
+    def depth_traversal(self, curr_node: OperationNode, visited: Set):
         '''
         Generate low-level requests (with no dependencies and hence high depth) first
         '''
         visited.add(curr_node.operation_id)
         for edge in curr_node.outgoing_edges:
             if edge.destination.operation_id not in visited:
-                self.naive_depth_traversal(edge.destination, visited)
-        self.naive_generate_request(curr_node.operation_properties)
+                self.depth_traversal(edge.destination, visited)
+        self.send_operation_request(curr_node.operation_properties)
 
-    def naive_generate_requests(self):
+    def generate_requests(self):
         '''
         Generate naive requests based on the operation graph
         '''
         visited = set()
         for operation_id, operation_node in self.operation_graph.operation_nodes.items():
             if operation_id not in visited:
-                self.naive_depth_traversal(operation_node, visited)
+                self.depth_traversal(operation_node, visited)
 
---
+++ src/value_generator.py
@@ -0,0 +1,92 @@
+import random
+import string
+from typing import Any, AnyStr, Dict
+
+from src.specification_parser import OperationProperties, SchemaProperties, ParameterProperties
+
+def randomize_boolean():
+    return random.choice([True, False])
+
+def randomize_null():
+    return None
+
+def randomize_integer(reasonable_range):
+    if random.randint(1, 100) <= 70:
+        return random.randint(0, 1000)
+    else:
+        return random.randint(-(2 ** 10), (2 ** 10))
+
+def randomize_float():
+    if random.randint(1, 100) <= 70:
+        return random.uniform(0, 1000)
+    else:
+        return random.uniform(-(2 ** 10), (2 ** 10))
+
+def randomize_string():
+    if random.randint(1, 100) <= 90:
+        length = random.randint(4, 16)
+    else:
+        length = random.randint(1, 100)
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+def randomize_array():
+    if random.randint(1, 100) <= 90:
+        length = random.randint(4, 10)
+    else:
+        length = random.randint(0, 50)
+    return [random.randint(-9999, 9999) for _ in range(length)]
+
+def randomize_object():
+    if random.randint(1, 100) <= 90:
+        length = random.randint(4, 10)
+    else:
+        length = random.randint(0, 50)
+    return {random.choice(string.ascii_letters): random.randint(-9999, 9999) for _ in range(length)}
+
+def randomized_array_length():
+    if random.randint(0, 100) <= 90:
+        return random.randint(4, 10)
+    else:
+        return random.randint(0, 50)
+
+def identify_generator(value: Any):
+    generators = {"integer": randomize_integer,
+                  "float": randomize_float,
+                  "number": randomize_float,
+                  "boolean": randomize_boolean,
+                  "string": randomize_string,
+                  "array": randomize_array,
+                  "object": randomize_object,
+                  "null": randomize_null}
+    return generators.get(value)
+
+class NaiveValueGenerator:
+    def __init__(self, parameters: Dict[AnyStr, ParameterProperties], request_body: Dict[AnyStr, SchemaProperties]):
+        self.parameters: Dict[AnyStr, ParameterProperties] = parameters
+        self.request_body: Dict[AnyStr, SchemaProperties] = request_body
+
+    def generate_value(self, item_properties: SchemaProperties) -> Any:
+        if item_properties.type == "object":
+            return {item_name: self.generate_value(item_properties) for item_name, item_properties in item_properties.properties.items()}
+        if item_properties.type == "array":
+            return [self.generate_value(item_properties.items) for _ in range(randomized_array_length())]
+        return identify_generator(item_properties.type)()
+
+    #def generate_values(self, parameters: Dict[AnyStr, ParameterProperties], request_body: Dict[AnyStr, SchemaProperties]):
+    ##    parameter_mappings = {}
+    #    request_body = None
+    #    for parameter in parameters.items()
+
+    def generate_parameters(self) -> Dict[AnyStr, Any]:
+        query_parameters = {}
+        for parameter_name, parameter_properties in self.parameters.items():
+            randomized_value = self.generate_value(parameter_properties.schema)
+            query_parameters[parameter_name] = randomized_value
+        return query_parameters
+
+    def generate_request_body(self):
+        request_properties = {}
+        for item_name, item_properties in self.request_body.items():
+            randomized_value = self.generate_value(item_properties)
+            request_properties[item_name] = randomized_value # save diff mime types
+        return request_properties
